2d0380b98ced9918724cb81d683717889435ecbc
MichelKachouh <michelkachouh06@gmail.com>
2025-11-05 17:38:26 +0200

    80% done of the menu finalized some details and about and contact page are done

--- JS/HomeScript.js
+++ JS/HomeScript.js
@@ -1,4 +1,4 @@
-/*const intro = document.getElementById('intro');
+const intro = document.getElementById('intro');
     const main = document.getElementById('main-content');
     const video = document.getElementById('intro-video');
 
@@ -8,4 +8,16 @@
         intro.style.display = 'none';
         main.style.display = 'block';
       }, 1000); // Matches fadeOut duration
-    };*/
+    };
+
+
+document.querySelectorAll('a').forEach(link => {
+  link.addEventListener('click', e => {
+    if (link.href.includes('#') || link.target === '_blank') return;
+      e.preventDefault();
+      document.body.classList.add('fade-out');
+      setTimeout(() => {
+        window.location = link.href;
+      }, 400);
+    });
+});
