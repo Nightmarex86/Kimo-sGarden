0ca1d457057427f02e3bc2fdf9bdcd572e7dc56b
MichelKachouh <michelkachouh06@gmail.com>
2026-02-09 09:26:17 +0200

    fixing phase

--- JS/HomeScript.js
+++ JS/HomeScript.js
@@ -1,16 +1,3 @@
-const intro = document.getElementById('intro');
-    const main = document.getElementById('main-content');
-    const video = document.getElementById('intro-video');
-
-    video.onended = () => {
-        intro.classList.add('fade-out');
-        setTimeout(() => {
-        intro.style.display = 'none';
-        main.style.display = 'block';
-      }, 1000); // Matches fadeOut duration
-    };
-
-
 document.querySelectorAll('a').forEach(link => {
   link.addEventListener('click', e => {
     if (link.href.includes('#') || link.target === '_blank') return;
